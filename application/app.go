4d189fa1653cf6c9e0a7afca0bbfd3e6a327eb47
Felix K <146659357+SomeCatCode@users.noreply.github.com>
2025-06-10 18:32:56 +0200

    add mongodb client

--- application/app.go
+++ application/app.go
@@ -4,31 +4,55 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type App struct {
 	router http.Handler
 
-	db *mongo.Client
+	db         *mongo.Database
+	collection *mongo.Collection
 }
 
 func New() *App {
-	// client, err := mongo.Connect(options.Client().ApplyURI(""))
-	// if err != nil {
-	// 	panic(fmt.Sprintf("failed to connect to MongoDB: %v", err))
-	// }
-
 	return &App{
-		router: loadRoutes(),
-		db:     nil,
+		router:     loadRoutes(),
+		db:         nil,
+		collection: nil,
 	}
+}
+
+func (a *App) ConnectMongo(ctx context.Context) error {
+	uri := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("MONGO_DB")
+	if uri == "" || dbName == "" {
+		return fmt.Errorf("MONGO_URI oder MONGO_DB nicht gesetzt")
+	}
+
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		return fmt.Errorf("MongoDB-Verbindung fehlgeschlagen: %w", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return fmt.Errorf("MongoDB-Ping fehlgeschlagen: %w", err)
+	}
+
+	a.db = client.Database(dbName)
+	return nil
 }
 
 func (a *App) Start(ctx context.Context) error {
 	var err error
+
+	if err = a.ConnectMongo(ctx); err != nil {
+		return err
+	}
+	defer a.db.Client().Disconnect(ctx)
 
 	server := &http.Server{
 		Addr:    ":8080",
