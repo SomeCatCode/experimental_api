c4a1afdb1b20ab4df15898ef4e121d9c280f78dd
Felix K <146659357+SomeCatCode@users.noreply.github.com>
2025-06-10 18:40:43 +0200

    add model definition

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 require (
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
--- model/organisation.go
+++ model/organisation.go
@@ -1,9 +1,29 @@
 package model
 
-type Organisation struct {
-	ID   string `json:"id" bson:"_id"`
-	Name string `json:"name" bson:"name"`
+import (
+	"time"
 
-	CreatedAt string `json:"created_at" bson:"created_at"`
-	UpdatedAt string `json:"updated_at" bson:"updated_at"`
+	"github.com/google/uuid"
+)
+
+type Organisation struct {
+	ID   string    `json:"id" bson:"_id"`
+	UUID uuid.UUID `json:"uid" bson:"uid"`
+
+	ParentId string `json:"parent_id" bson:"parent_id"`
+
+	Name        string `json:"name" bson:"name"`
+	Description string `json:"description" bson:"description"`
+
+	Homepage string `json:"website_url" bson:"website_url"`
+	Email    string `json:"contact_email" bson:"contact_email"`
+	Phone    string `json:"contact_phone" bson:"contact_phone"`
+	Address  string `json:"address" bson:"address"`
+
+	Latetude  float64 `json:"latitude" bson:"latitude"`
+	Longitude float64 `json:"longitude" bson:"longitude"`
+
+	Style     string     `json:"style" bson:"style"`
+	CreatedAt *time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at" bson:"updated_at"`
 }
