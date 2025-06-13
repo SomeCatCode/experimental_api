39628bd7f7a1f8a7d7effa286b857ad463e704ea
Felix K <it@wasmitleder.de>
2025-06-13 07:42:24 +0200

    change Model

--- model/organisation.go
+++ model/organisation.go
@@ -2,23 +2,19 @@ package model
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type Organisation struct {
-	ID   string    `json:"id" bson:"_id"`
-	UUID uuid.UUID `json:"uid" bson:"uid"`
+	ID             string `json:"id" bson:"_id"`
+	OrganisationId string `json:"organisation_id" bson:"organisation_id"`
 
-	ParentId string `json:"parent_id" bson:"parent_id"`
+	Name        string  `json:"name" bson:"name"`
+	Description string  `json:"description" bson:"description"`
+	Address     Address `json:"address" bson:"address"`
 
-	Name        string `json:"name" bson:"name"`
-	Description string `json:"description" bson:"description"`
-
-	Homepage string `json:"website_url" bson:"website_url"`
-	Email    string `json:"contact_email" bson:"contact_email"`
-	Phone    string `json:"contact_phone" bson:"contact_phone"`
-	Address  string `json:"address" bson:"address"`
+	Phone    string `json:"phone" bson:"phone"`
+	Email    string `json:"email" bson:"email"`
+	Homepage string `json:"homepage" bson:"homepage"`
 
 	Latetude  float64 `json:"latitude" bson:"latitude"`
 	Longitude float64 `json:"longitude" bson:"longitude"`
@@ -27,4 +23,12 @@ type Organisation struct {
 
 	CreatedAt *time.Time `json:"created_at" bson:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at" bson:"updated_at"`
+}
+
+type Address struct {
+	Street  string `json:"street" bson:"street"`
+	City    string `json:"city" bson:"city"`
+	State   string `json:"state" bson:"state"`
+	Country string `json:"country" bson:"country"`
+	Postal  string `json:"postal" bson:"postal"`
 }
